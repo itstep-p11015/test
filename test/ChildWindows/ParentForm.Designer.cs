3f7eee2b4a3341ba1d67db804ffc0fed4937eddc
user <user@DESKTOP-0MIU59N>
2016-10-31 07:56:38 +0300

    Change form text :D

--- test/ChildWindows/ParentForm.Designer.cs
+++ test/ChildWindows/ParentForm.Designer.cs
@@ -112,7 +112,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "ParentForm";
-            this.Text = "Form";
+            this.Text = "Form:)";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
